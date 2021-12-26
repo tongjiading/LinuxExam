73ee4d715f0eec2d686aae856a1f5a4142bfff49
“tongjiading” <tongjiading@outlook.com>
2021-12-26 10:07:14 +0800

    我再再次上传了LinuxExam结课作业

---
+++ LinuxExam/LinuxExam/LinuxExam/Query.java
@@ -0,0 +1,131 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import redis.clients.jedis.Jedis;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/query")
+public class Query extends HttpServlet {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String URL =
+            "jdbc:mysql://120.48.0.79/bookmanager?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B8";
+    static final String USER = "root";
+    static final String PASS = "!Tong0130";
+    static final String SQL_QUERY_BOOK = "SELECT * FROM book;";
+
+    static Connection conn = null;
+    static Jedis jedis = null;
+
+    public void init() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            jedis = new Jedis("120.48.0.79");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void destroy() {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+
+
+        String bookList = jedis.get("booklist");
+
+        if (bookList == null){
+            List<Book> list = getStudent();
+            Gson gson = new Gson();
+            String json = gson.toJson(list, new TypeToken<List<Book>>() {}.getType());
+            jedis.set("booklist",json);
+            out.println(json);
+            System.out.println("走数据库，存入缓存");
+        }else{
+            out.println(bookList);
+            System.out.println("走缓存");
+        }
+
+        out.flush();
+        out.close();
+
+    }
+
+
+    private List<Book> getStudent() {
+        List<Book> list = new ArrayList<Book>();
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL_QUERY_BOOK);
+            while (rs.next()) {
+                Book book = new Book();
+                book.setId(rs.getString("id"));
+                book.setName(rs.getString("name"));
+                book.setPrice(rs.getString("price"));
+                list.add(book);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return list;
+    }
+
+    class Book {
+        private String id;
+        private String name;
+        private String price;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+    }
+
+}
+
